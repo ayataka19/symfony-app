017df4213dee2fce7cb8d83e33b0fd9877b79ac7
takashi.aoyagi <takashi.aoyagi@geniee.co.jp>
2019-05-17 21:07:11 +0900

    update: クエリパラメーターの利用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}/{pass}", name="hello")
+     * @Route("/hello", name="hello")
      */
-    public function index($name='(no name)', $pass='(no paasword)')
+    public function index(Request $request)
     {
+        $name = $request->get('name');
+        $pass = $request->get('pass');
         $result = '<html><body><ol>';
         $result .= '<h1>Parameter</h1>';
         $result .= '<p>name: ' . $name . '</p>';
