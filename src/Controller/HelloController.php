63b4ead80d489ea73616c6cb2edf5f097cc9d84c
takashi.aoyagi <takashi.aoyagi@geniee.co.jp>
2019-05-22 22:20:03 +0900

    add: JSONログ出力

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,14 +1,29 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
+    /**
+     * @Route("/log", name="log")
+     */
+    public function log(Request $request, LoggerInterface $logger)
+    {
+        $data = array (
+            'name'=>array('first'=>'Taro','second'=>'Yamada'),
+            'age'=>36, 'mail'=>'taro@yamada.kun'
+        );
+        $logger->info(serialize($data));
+        return new JsonResponse($data);
+    }
+
     /**
      * @Route("/hello", name="hello")
      */
