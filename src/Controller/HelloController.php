2696df85f0a9f5834d3ecec7ba031cf094538953
takashi.aoyagi <takashi.aoyagi@geniee.co.jp>
2019-06-07 18:15:18 +0900

    add: レコードの作成・更新・削除

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 
 class HelloController extends AbstractController
@@ -57,6 +58,96 @@ class HelloController extends AbstractController
             'title' => 'Hello',
             'data' => $person,
         ]);
+    }
+
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request)
+    {
+        $person = new Person();
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, array('label' => 'Click'))
+            ->getForm();
+
+
+        if ($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($person);
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig', [
+                'title' => 'Hello',
+                'message' => 'Create Entity',
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+
+
+    /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update(Request $request, Person $person)
+    {
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, array('label' => 'Click'))
+            ->getForm();
+
+
+        if ($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig', [
+                'title' => 'Hello',
+                'message' => 'Update Entity id=' . $person->getId(),
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Request $request, Person $person)
+    {
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, array('label' => 'Click'))
+            ->getForm();
+
+
+        if ($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($person);
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig', [
+                'title' => 'Hello',
+                'message' => 'Delete Entity id=' . $person->getId(),
+                'form' => $form->createView(),
+            ]);
+        }
     }
 
 
