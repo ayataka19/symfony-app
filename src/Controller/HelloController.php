48fae6e16275e216862e933058219de866a309ea
takashi.aoyagi <takashi.aoyagi@geniee.co.jp>
2019-06-07 22:01:18 +0900

    add: findByでレコード検索

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -54,13 +54,30 @@ class HelloController extends AbstractController
 
 
     /**
-     * @Route("/find/{id}", name="find")
+     * @Route("/find", name="find")
      */
-    public function find(Request $request, Person $person)
+    public function find(Request $request)
     {
+        $formobj = new FindForm();
+        $form = $this->createFormBuilder($formobj)
+            ->add('find', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Click'))
+            ->getForm();
+
+
+        if ($request->getMethod() == 'POST'){
+            $form->handleRequest($request);
+            $findstr = $form->getData()->getFind();
+            $repository = $this->getDoctrine()
+                ->getRepository(Person::class);
+            $result = $repository->findBy(['name' => $findstr]);
+        } else {
+            $result = null;
+        }
         return $this->render('hello/find.html.twig', [
             'title' => 'Hello',
-            'data' => $person,
+            'form' => $form->createView(),
+            'data' => $result,
         ]);
     }
 
